b6d16f41edc0ef587edae9cd5a1e1828c203cedc
Emily Rosengren <erosengr@thoughtworks.com>
2013-02-26 21:24:54 -0600

    Hugo/Emily don't make request if we've hit twitter api limit -- needs refactoring

--- twitter_cache.rb
+++ twitter_cache.rb
@@ -5,8 +5,23 @@ require 'sinatra'
 CACHE = Cache.new(15 * 60)
 
 get '/*' do
- query = "#{params[:splat].first}?q=#{params["q"]}"
- request= TwitterRequest.new(CACHE, query) 
- request.response.body
+  @limit_remaining ||= 1
+  @limit_reset ||= 1
+
+  query = "#{params[:splat].first}?q=#{params["q"]}"
+  if can_make_request?
+    request = TwitterRequest.new(CACHE, query) 
+    response = request.response
+    if response.code == 200
+      @limit_remaining = response.header['X-Rate-Limit-Remaining']
+      @limit_reset = response.header['X-Rate-Limit-Reset']
+    end
+    response.body
+  else
+    %Q{{"errors": [{"code": 88, "message": "Rate limit exceeded"}]}}
+  end
 end
 
+def can_make_request?
+  @limit_remaining > 0 || Time.now.utc.to_i > @limit_reset
+end
